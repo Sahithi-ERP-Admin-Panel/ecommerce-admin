98d9012ff1c0105eb318aeeb6a76e137167ddd43
baludangeti <baludangeti1@gmail.com>
2024-10-01 09:23:59 +0530

    balu chages

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -28,6 +28,18 @@ const SideBar = ()=>{
                         <span class="align-middle">{messages.orders_label}</span>
 	                    </a>
 					</li>
+					<li class="sidebar-item">
+						<a href="/pages" data-bs-target="#pages" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg> <span class="align-middle">{messages.pages_label}</span>
+						</a>
+						<ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar" >
+							<li class="sidebar-item active"><Link class="sidebar-link" to="/dashboard">{messages.default_label}</Link></li>
+							<li class="sidebar-item active"><Link class="sidebar-link" to="/dashboard">{messages.default_label}</Link></li>
+							<li class="sidebar-item active"><Link class="sidebar-link" to="/dashboard">{messages.default_label}</Link></li>
+						</ul>
+
+					</li>
+					
 					
 				</ul>
 
